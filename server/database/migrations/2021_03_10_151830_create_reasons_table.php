e4dcff5f4132c91121a65c36c036e17f1a8fad6c
unknown <erik.galbero@gmail.com>
2021-03-18 10:31:32 +0100

    fix #3 add reasons migration and fix reason table

--- server/database/migrations/2021_03_10_151830_create_reasons_table.php
+++ server/database/migrations/2021_03_10_151830_create_reasons_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateReasonsTable extends Migration
@@ -16,7 +17,9 @@ class CreateReasonsTable extends Migration
         Schema::create('reasons', function (Blueprint $table) {
             $table->uuid('id')->unique()->primary();
             $table->string('text');
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
         });
     }
 
